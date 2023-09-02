cffb842584811076419b98cea8a90c991508cf42
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2023-09-02 22:06:09 +0300

    adds pagination redirect  for slider component

--- src/features/slider/ui/slider.tsx
+++ src/features/slider/ui/slider.tsx
@@ -6,10 +6,20 @@ import arrowRight from '../assets/arrow_right.svg'
 import SliderContentContainer from "@/features/slider/components/container/ui/slider-content-container";
 import {SLIDER_ITEM} from "@/features/slider/const/slider-item";
 import {useState} from "react";
+import Link from "next/link";
 
 export default function Slider () {
     const [currentSlider,setCurrentSlider] = useState(0)
-    const sliderContent = SLIDER_ITEM[currentSlider]
+    const SLIDER_ITEMS = SLIDER_ITEM.slice()
+    const sliderContent = SLIDER_ITEMS[currentSlider]
+    const redirectSliderItem = (item:string) => {
+        const redirectItem = SLIDER_ITEMS.findIndex(
+            (index) => index.id === item
+
+        )
+       setCurrentSlider(redirectItem)
+
+    }
     const incrementSliderItem = () => {
         const firstSliderItem = currentSlider === 0
         const newIndex = firstSliderItem || currentSlider < SLIDER_ITEM.length - 1 ? currentSlider + 1 : 0
@@ -25,7 +35,23 @@ export default function Slider () {
 
         <div className={slider.container}>
             <Image src={arrowLeft} alt={'arrow-left'} onClick={decrementSliderItem}/>
-                <SliderContentContainer  props={sliderContent}/>
+                <div className={slider.containerSliderContent}>
+                    <SliderContentContainer  props={sliderContent}/>
+                    <ul className={slider.pagination} >
+                        {SLIDER_ITEMS.map((item) =>
+                            <li className={slider.count}
+                                key={item.id}
+                                onClick={
+                                    () => {
+                                        redirectSliderItem(item.id)
+                                    }
+                            }>
+                                {item.id}
+                            </li>
+                        )}
+                    </ul>
+
+                </div>
             <Image src={arrowRight} alt={'arrow-right'} onClick={incrementSliderItem}/>
         </div>
     )
