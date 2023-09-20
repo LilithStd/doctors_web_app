ebfd91f5e382699e294030da8455eaeb8317d028
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2023-09-20 21:58:08 +0300

    add routing for page articles and tests, create articles component

--- src/features/navbar/ui/navbar.tsx
+++ src/features/navbar/ui/navbar.tsx
@@ -2,11 +2,17 @@ import navbar from '@/features/navbar/style/navbar.module.scss'
 import {NAVBAR_ITEMS} from "@/features/navbar/const/navbar_items";
 import {Title} from "@/shared/ui/title/title";
 import {NavbarPropsItemTitle} from "@/features/navbar/const/navbarProps";
+import Link from "next/link";
 export default function Navbar () {
     return  (
 		<ul className={navbar.container}>
 			{NAVBAR_ITEMS.map((item)=>
-					<li key={item} className={navbar.item}>{<Title size={NavbarPropsItemTitle.TITLE_SIZE} content={item}/>}</li>
+					<li key={item.name} className={navbar.item}>{
+						<Link href={`/${item.link}`}>
+							<Title size={NavbarPropsItemTitle.TITLE_SIZE} content={item.name}/>
+						</Link>
+						}
+					</li>
 			)}
 		</ul>
     )
